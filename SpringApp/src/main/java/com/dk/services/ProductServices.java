b10668c7c944b64e2dd0370147c4a785bcb63bad
Dikhang1112 <duykhanggt5@gmail.com>
2025-03-23 16:17:24 +0700

    Buoi th 3

---
+++ SpringApp/src/main/java/com/dk/services/ProductServices.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.dk.services;
+
+import com.dk.pojo.Product;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author admin
+ */
+public interface ProductServices {
+    public interface ProductService {
+    List<Product> getProducts(Map<String, String> params);
+    Product saveOrUpdate(Product p);
+    Product getProductById(int id);
+}
+}
---
+++ SpringApp/src/main/java/com/dk/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dk.services.impl;
+
+import com.dk.pojo.Category;
+import com.dk.repositories.CategoryRepository;
+import com.dk.services.CategoryServices;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class CategoryServiceImpl implements CategoryServices {
+    @Autowired
+    private CategoryRepository cateRepo;
+
+    @Override
+    public List<Category> getCates() {
+        return this.cateRepo.getCates();
+    }
+    
+}
